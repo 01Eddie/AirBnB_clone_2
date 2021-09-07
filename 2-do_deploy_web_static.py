8e9289511a4b5472736824f68b04a10c00ba36ec
01Eddie <jesus.11.97@hotmail.com>
2021-09-06 22:59:43 -0500

    [FIX] task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ from os import path
 env.hosts = ['35.237.172.160', '35.237.222.103']
 env.user = 'ubuntu'
 
+
 def do_deploy(archive_path):
     """ * Prototype: def do_deploy(archive_path):
     - depending of your implementation of it, you may don’t need it """
@@ -19,14 +20,15 @@ def do_deploy(archive_path):
         NameArchive = archive_path.split('/')[-1]
         NameArchiveWitoutExtension = NameArchive.replace('.tgz', '')
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/" + NameArchiveWitoutExtension)
-        run("sudo tar -xzvf /tmp/" + NameArchive + " -C /data/web_static/releases/")
+        dr = "/data/web_static/releases/"
+        run("sudo mkdir -p " + dr + NameArchiveWitoutExtension)
+        run("sudo tar -xzvf /tmp/" + NameArchive + " -C " + dr)
         run("sudo rm -rf /tmp/" + NameArchive)
-
-        run("sudo mv /data/web_static/releases/web_static/* /data/web_static/releases/")
-        run("sudo rm -rf /data/web_static/releases/web_static")
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/ /data/web_static/current")
+        r = "/data/web_static/"
+        run("sudo mv " + r + "releases/web_static/* " + r + "releases/")
+        run("sudo rm -rf " + r + "releases/web_static")
+        run("sudo rm -rf " + r + "current")
+        run("sudo ln -sf " + r + "releases/ " + r + "current/")
 
         return True
     except Exception:
