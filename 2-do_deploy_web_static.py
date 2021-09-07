b980b8877e14f7ef7e7567a4a5a6450bb2e6565e
01Eddie <jesus.11.97@hotmail.com>
2021-09-07 00:34:51 -0500

    [fix] task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 1-pack_web_static.py) that distributes an
-archive to your web servers"""
-import fabric
+"""
+Send file to your servers with fabric
+"""
+
 from fabric.api import *
 from datetime import datetime
 from os import path
 
 env.hosts = ['35.237.172.160', '35.237.222.103']
-env.user = 'ubuntu'
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """ * Prototype: def do_deploy(archive_path):
-    - depending of your implementation of it, you may don’t need it """
+    """ Deploy the file in specific folders in the servers """
+    if path.isfile(archive_path) is False:
+        return False
+    # With .tgz
+    filetgz = archive_path.split("/")[-1]
+    # No .tgz
+    filename = filetgz.replace('.tgz', '')
 
-    """if not path.exists(archive_path):
-        return False"""
+    newdir = "/data/web_static/releases/" + filename
+
     try:
-        NameArchive = archive_path.split('/')[-1]
-        NameArchiveWitoutExtension = NameArchive.replace('.tgz', '')
         put(archive_path, "/tmp/")
-        dr = "/data/web_static/releases/"
-        run("sudo mkdir -p " + dr + NameArchiveWitoutExtension)
-        run("sudo tar -xzvf /tmp/" + NameArchive + " -C " + dr)
-        run("sudo rm -rf /tmp/" + NameArchive)
-        r = "/data/web_static/"
-        run("sudo mv " + r + "releases/web_static/* " + r + "releases/")
-        run("sudo rm -rf " + r + "releases/web_static")
-        run("sudo rm -rf " + r + "current")
-        run("sudo ln -sf " + r + "releases/ " + r + "current/")
-
+        run("sudo mkdir -p {}/".format(newdir))
+        run("sudo tar -xzf /tmp/{} -C {}/".format(filetgz, newdir))
+        run("sudo rm /tmp/{}".format(filetgz))
+        """ sudo cp -r web_static/releases/web_static_20210906234551/web_static
+        /* /data/web_static/releases/web_static_20210906234551/ && sudo rm -R
+        web_static/releases/web_static_20210906234551/web_static/*
+        run("sudo cp -r {}/web_static/* {}/ && sudo rm -R {}".format(newdir,
+        newdir, newdir))"""
+        run("sudo mv {}/web_static/* {}/".format(newdir, newdir))
+        run("sudo rm -rf {}/web_static".format(newdir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newdir))
+        print("YOU DID IT BOY!")
         return True
     except Exception:
         return False
---
+++ 2-do_deploy_web_static01.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Fabric script (based on the file 1-pack_web_static.py) that distributes an
+archive to your web servers"""
+import fabric
+from fabric.api import *
+from datetime import datetime
+from os import path
+
+env.hosts = ['35.237.172.160', '35.237.222.103']
+env.user = 'ubuntu'
+
+def do_deploy(archive_path):
+    """ * Prototype: def do_deploy(archive_path):
+    - depending of your implementation of it, you may don’t need it """
+
+    if not path.exists(archive_path):
+        return False
+    try:
+        NameArchive = archive_path.split('/')[-1]
+        NameArchiveWitoutExtension = NameArchive.replace('.tgz', '')
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p /data/web_static/releases/" + NameArchiveWitoutExtension)
+        run("sudo tar -xzvf /tmp/" + NameArchive + " -C /data/web_static/releases/")
+        run("sudo rm -rf /tmp/" + NameArchive)
+
+        run("sudo mv /data/web_static/releases/" + NameArchiveWitoutExtension + "/web_static/* /data/web_static/releases/" + NameArchiveWitoutExtension + "/")
+        run("sudo rm -rf /data/web_static/releases/web_static")
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf /data/web_static/releases/ /data/web_static/current")
+
+        return True
+    except Exception:
+        return False
